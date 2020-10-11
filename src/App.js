33d16d3d50479d6bb8f90998ee83c0b9f40f8f51
grace <grace.c.youngblood@gmail.com>
2020-10-11 13:02:49 -0400

    drop logo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from "styled-components"
-import logo from './logo.svg';
 
 function App() {
   return (
     <Application>
       <AppHeader>
-        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,11 +19,6 @@ function App() {
     </Application>
   );
 }
-
-const AppLogo = styled.img`
-height: 40vmin;
-pointer-events: none;
-`
 
 const AppHeader = styled.header`
 background-color: #282c34;
@@ -44,22 +37,6 @@ color: #61dafb;
 
 const Application = styled.div`
 text-align: center;
-
-@media (prefers-reduced-motion: no-preference) {
-  ${AppLogo} {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
 `
 
 export default App;
