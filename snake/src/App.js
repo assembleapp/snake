af31d7feb4b1d0f09dd9d53d8f49446eb47a6b25
grace <grace.c.youngblood@gmail.com>
2020-10-11 12:55:57 -0400

    basic styled-components refactor.

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
+import styled from "styled-components"
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Application>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </Application>
   );
 }
+
+const AppLogo = styled.img`
+height: 40vmin;
+pointer-events: none;
+`
+
+const AppHeader = styled.header`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+`
+
+const AppLink = styled.a`
+color: #61dafb;
+`
+
+const Application = styled.div`
+text-align: center;
+
+@media (prefers-reduced-motion: no-preference) {
+  ${AppLogo} {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+`
 
 export default App;
